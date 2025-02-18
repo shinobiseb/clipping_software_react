9531782f72d64d0457ea3e4ab9203ec9ad23ba24
Sebastian Patin <sebpatin@gmail.com>
2025-02-18 15:53:22 -0500

    Range Slider from react-range-slider-input working - deleted old library (too ugly)

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,5 +1,6 @@
-import { ChangeEvent, useRef, useState } from 'react';
-import MultiRangeSlider, { ChangeResult } from 'multi-range-slider-react';
+import { ChangeEvent, ChangeEventHandler, useRef, useState } from 'react';
+import RangeSlider, { InputEventHandler } from 'react-range-slider-input';
+import 'react-range-slider-input/dist/style.css';
 
 type SliderProps = {
   videoLength: number;
@@ -30,30 +31,13 @@ export default function Slider({ videoLength, setTimestamps }: SliderProps) {
   }
 
 
-  function handleSlider(e: ChangeEvent<HTMLInputElement>) {
-    const ms = Number(e.target.value);
-    const time = msToTime(ms);
+  function handleRange(value: [number, number]) {
+    let start = msToTime(value[0]);
+    let end = msToTime(value[1]);
 
-    console.log(time)
+    console.log("Start: ", start, "End: ", end)
 
-    if (e.target.id === 'startSlider') {
-      setVideoTimeStampFunction('startTime', time, setTimestamps);
-    } else if (e.target.id === 'endSlider') {
-      setVideoTimeStampFunction('endTime', time, setTimestamps);
-    }
-  }
-
-  function handleRange(e: ChangeResult){
-    let start = msToTime(e.minValue);
-    let end = msToTime(e.maxValue);
-
-    setTimestamps((prev) => {
-      if (prev.startTime === start && prev.endTime === end) {
-        return prev; // Prevents re-render if values haven't changed
-      }
-      return { startTime: start, endTime: end };
-    })
-    console.log(start, end)
+    setTimestamps({ startTime: start, endTime: end })
   }
 
   function msToTime(duration: number) {
@@ -71,7 +55,14 @@ export default function Slider({ videoLength, setTimestamps }: SliderProps) {
 
   return (
     <div className="w-full justify-center flex flex-col items-center">
-      <MultiRangeSlider
+      <RangeSlider
+        min={0}
+        max={videoLength * 1000}
+        onInput={handleRange}
+        step={1}
+        className='py-5 my-2'
+      />
+      {/* <MultiRangeSlider
         label='false'
         ruler='false'
         canMinMaxValueSame={false}
@@ -84,7 +75,7 @@ export default function Slider({ videoLength, setTimestamps }: SliderProps) {
         barRightColor='white'
         barInnerColor='green'
         id='Slider'
-      />
+      /> */}
       {/* <input
         ref={startRef}
         onChange={handleSlider}
