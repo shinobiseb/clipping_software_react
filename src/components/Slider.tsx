1bb5b4385b439ff1fa7f3a71e4db1f379c7dfe47
Sebastian Patin <sebpatin@gmail.com>
2025-02-18 17:24:28 -0500

    update

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,5 +1,5 @@
-import { ChangeEvent, ChangeEventHandler, useRef, useState } from 'react';
-import RangeSlider, { InputEventHandler } from 'react-range-slider-input';
+import { useEffect, useState } from 'react';
+import RangeSlider from 'react-range-slider-input';
 import 'react-range-slider-input/dist/style.css';
 
 type SliderProps = {
@@ -13,14 +13,19 @@ type SliderProps = {
 };
 
 export default function Slider({ videoLength, setTimestamps }: SliderProps) {
+  const [range, setRange] = useState<[number, number]>([0, videoLength * 1000]);
+
+  // Update range when videoLength changes
+  useEffect(() => {
+    setRange([0, videoLength * 1000]);
+  }, [videoLength]);
 
   function handleRange(value: [number, number]) {
-    let start = msToTime(value[0]);
-    let end = msToTime(value[1]);
+    const start = msToTime(value[0]);
+    const end = msToTime(value[1]);
 
-    console.log("Start: ", start, "End: ", end)
-
-    setTimestamps({ startTime: start, endTime: end })
+    setRange(value); // Update state
+    setTimestamps({ startTime: start, endTime: end });
   }
 
   function msToTime(duration: number) {
@@ -29,21 +34,18 @@ export default function Slider({ videoLength, setTimestamps }: SliderProps) {
     let minutes: string = Math.floor((duration / (1000 * 60)) % 60).toString();
     let hours: string = Math.floor((duration / (1000 * 60 * 60)) % 24).toString();
 
-    hours = hours.padStart(2, '0');
-    minutes = minutes.padStart(2, '0');
-    seconds = seconds.padStart(2, '0');
-
-    return `${hours}:${minutes}:${seconds}.${milliseconds}`;
+    return `${hours.padStart(2, '0')}:${minutes.padStart(2, '0')}:${seconds.padStart(2, '0')}.${milliseconds}`;
   }
 
   return (
-    <div className="w-full justify-center flex flex-col items-center">
+    <div className="w-full flex flex-col items-center">
       <RangeSlider
         min={0}
         max={videoLength * 1000}
         onInput={handleRange}
+        value={range}
         step={1}
-        className='py-5 my-2'
+        className="py-5 my-2"
       />
     </div>
   );
