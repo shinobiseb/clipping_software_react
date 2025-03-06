808facce8cb0274c3de7e6d2c8784f9bb966e25c
shinobiseb <sebpatin@gmail.com>
2025-03-06 17:59:39 -0500

    Quick Update

--- src/components/WebAsem.tsx
+++ src/components/WebAsem.tsx
@@ -68,6 +68,10 @@ export default function WebAsem() {
     }
   }, [activeThumb, timeStampSeconds]);
 
+  useEffect(()=> {
+    console.log("MetaData Loaded?", isMetaDataLoaded)
+  }, [isMetaDataLoaded])
+
   let timeoutID : ReturnType<typeof setTimeout> | undefined;
 
   // --------- Functions --------- //
